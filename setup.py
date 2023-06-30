9095278c837ef9de793ff0fb913a7784b3686c36
Langston Nashold <lgnashold@gmail.com>
2023-06-29 20:01:57 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='playgroundrl_envs',
-   version='0.0.5',
-   author='Rayan Krishnan',
-   # TODO: figure out systematic way to do this
-   packages=['playgroundrl_envs', 'playgroundrl_envs.games', 'playgroundrl_envs.games.go', 'playgroundrl_envs.games.go.engine', 'playgroundrl_envs.games.codenames', 'playgroundrl_envs.games.catan'],
-   package_dir={'':'src'},
-   scripts=[],
-   url='http://pypi.python.org/pypi/playgroundrl_envs/',
-   description='The environments hosted on Playground RL',
-   # long_description=open('README.txt').read(),
-   install_requires=[
-        'attrs==22.2.0',
-        'cattrs==22.2.0',
-   ],
+    name="playgroundrl_envs",
+    version="0.0.5",
+    author="Rayan Krishnan",
+    # TODO: figure out systematic way to do this
+    packages=[
+        "playgroundrl_envs",
+        "playgroundrl_envs.games",
+        "playgroundrl_envs.games.go",
+        "playgroundrl_envs.games.go.engine",
+        "playgroundrl_envs.games.codenames",
+        "playgroundrl_envs.games.catan",
+    ],
+    package_dir={"": "src"},
+    scripts=[],
+    url="http://pypi.python.org/pypi/playgroundrl_envs/",
+    description="The environments hosted on Playground RL",
+    # long_description=open('README.txt').read(),
+    install_requires=[
+        "attrs==22.2.0",
+        "AutoROM==0.6.1",
+        "catanatron==3.2.1",
+        "cattrs==22.2.0",
+        "chess==1.9.4",
+        "gymnasium==0.28.1",
+        "numpy==1.24.2",
+        "pettingzoo==1.23.1",
+        "Pillow==9.5.0",
+        "pygame==2.4.0",
+        "scikit-learn==1.2.2",
+        "scipy==1.10.1",
+        "texasholdem==0.9.0",
+    ],
 )
