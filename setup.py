8c1c1fe7a0e3b94da62b9f9de116e66676cb0016
Langston Nashold <lgnashold@gmail.com>
2023-07-24 21:28:29 -0400

    Increment Version (#15)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="playgroundrl_envs",
-    version="0.0.11",
+    version="0.0.13",
     author="Rayan Krishnan",
     packages=find_packages(),
     package_dir={"": "src"},
